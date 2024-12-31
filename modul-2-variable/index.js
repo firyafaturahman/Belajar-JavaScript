0d445b004f446b3c56042020a57324431193ba6c
firya faturahman <firya.faturahman2017@gmail.com>
2024-12-31 22:30:08 +0700

    modul 2 variable

---
+++ modul-2-variable/index.js
@@ -0,0 +1,24 @@
+/** var, key untuk mengakses value baik di dalam scope maupun di luar scope */
+
+// if (true) {
+//   var nama = "firya";
+
+//   console.log("var:", nama);
+// }
+// console.log("var:", nama);
+
+/** let, key untuk mengakses value hanya di dalam scope-nya dan value bisa diubah */
+
+if (true) {
+  let nama = "firya";
+  nama = "faturahman";
+  console.log("let:", nama);
+}
+
+/** const, key untuk mengakses value hanya di dalam scope-nya dan value tidak bisa diubah */
+
+if (true) {
+  const nama = "asep";
+  // nama = "pesa";
+  console.log("const:", nama);
+}
